dd8c7ab996fd8babdd03a771f7a8e8d59ecb9c4d
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-08-05 12:35:53 +0800

    Increase faucet increase from 0.01 to 1

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -361,7 +361,7 @@ function App(props) {
           onClick={() => {
             faucetTx({
               to: address,
-              value: ethers.utils.parseEther("0.01"),
+              value: ethers.utils.parseEther("1"),
             });
             setFaucetClicked(true);
           }}
