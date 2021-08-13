daea650d198a4e58814012644b906137f9f4bbcb
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-08-13 22:47:07 +0800

    Change App Name

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="DeFi Together"
+        subTitle="Take back your financial freedom, TOGETHER"
         style={{ cursor: "pointer" }}
       />
     </a>
